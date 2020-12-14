a461b2700eda76b18ce078573d82814cb64de58e
Mathijs455 <73833862+Mathijs455@users.noreply.github.com>
2020-12-14 11:33:30 +0100

    Image Upload
    Checked nu ook op bestands lengte. en krijgt een repsonse terug als de api het bestand afkeurt

--- BramrSite/Pages/Components/ImageUpload.razor.cs
+++ BramrSite/Pages/Components/ImageUpload.razor.cs
@@ -20,27 +20,39 @@ namespace BramrSite.Pages.Components
         Stream FileStream { get; set; }
         private bool IsDisabled { get; set; } = true;
         private string ErrorMessage { get; set; }
-
+        private string FileName { get; set; }
 
         private async Task OpenImage()
         {
             var file = (await FileReader.CreateReference(FileReference).EnumerateFilesAsync()).FirstOrDefault();
-
-            if(file == null)
+            var fileInfo = await file.ReadFileInfoAsync();
+            
+            var foo = fileInfo.Name.Length;
+            FileName = fileInfo.Name.Substring(0, 10);
+            Console.WriteLine(FileName);
+            if (file == null)
             {
+                
+            }
+
+            Console.WriteLine(fileInfo.Size);
+            if (fileInfo.Size > 314572)
+            {
+                ErrorMessage = "Bestand mag niet groter zijn dan 3 mb.";
                 return;
             }
 
-            var fileInfo = await file.ReadFileInfoAsync();
-            if(fileInfo.Type == "image/png" || fileInfo.Type == "image/jpeg" || fileInfo.Type == "image/jpg")
+            if (fileInfo.Type == "image/png" || fileInfo.Type == "image/jpeg" || fileInfo.Type == "image/jpg")
             {
                 using var memoryStream = await file.CreateMemoryStreamAsync((int)fileInfo.Size);
                 FileStream = new MemoryStream(memoryStream.ToArray());
                 IsDisabled = false;
+                return;
             }
+
             else
             {
-                ErrorMessage = "Only accept files fo type: png, jpg, jpeg";
+              ErrorMessage = "Only accept files fo type: png, jpg, jpeg";
             }
         }
 
@@ -48,11 +60,18 @@ namespace BramrSite.Pages.Components
         {
             IsDisabled = true;
 
-            await Api.UploadImage(FileStream, CurrentImage.FileType.ToString());
+            var apiResponse = await Api.UploadImage(FileStream, CurrentImage.FileType.ToString());
             CurrentImage.FileUri = await Api.GetFileInfo(CurrentImage.FileType.ToString());
             CurrentImage.Src = $"https://localhost:44372/api/image/download/{CurrentImage.FileUri}";
             CallBackMethod(CallBack);
-            ErrorMessage = "Succesfully uploaded";
+            if(apiResponse.Success == true)
+            {
+                ErrorMessage = "Succesfully uploaded";
+            }
+            else
+            {
+                ErrorMessage = apiResponse.Message;
+            }
         }
 
         public void CallBackMethod(Del CallBackMethod)
