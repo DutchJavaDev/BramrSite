7cd826244e465281f5ffd3dc1204ea367492f93c
Boris <Borismulder@outlook.com>
2021-01-08 13:44:31 +0100

    Account page

---
+++ BramrSite/Pages/Account.razor.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using BramrSite.Models;
+using System.Collections.Generic;
+
+namespace BramrSite.Pages
+{
+    public partial class Account : ComponentBase
+    {
+        [Inject] IJSRuntime IJSRuntime { get; set; }
+        private IJSObjectReference Module;
+
+        private List<NoteModel> Notes { get; set; }
+        
+
+        protected override async void OnInitialized()
+        {
+            Notes = new List<NoteModel>();
+
+            Module = await IJSRuntime.InvokeAsync<IJSObjectReference>("import", "./js/AccountScript.js");
+
+            await Module.InvokeVoidAsync("Init");
+            await Module.InvokeVoidAsync("tabs",0);
+        }
+
+
+        public async void Tab(int tabIndex)
+        {
+            await Module.InvokeVoidAsync("tabs", tabIndex);
+        }
+
+        public void AddNote()
+        {
+            Notes.Add(new NoteModel {
+                EditMode = EditMode.Edit,
+                Text = ""
+            });
+        }
+
+        public void RemoveNote(NoteModel model)
+        {
+            Notes.Remove(model);
+        }
+    }
+}
---
+++ BramrSite/wwwroot/js/AccountScript.js
@@ -0,0 +1,15 @@
+﻿export function Init()
+{
+    $('.nav ul li').click(function () {
+        $(this).addClass("active").siblings().removeClass('active');
+    })
+}
+
+export function tabs(panelIndex) {
+    const tab = document.querySelectorAll('.tab');
+
+    tab.forEach(function (node) {
+        node.style.display = 'none';
+    });
+    tab[panelIndex].style.display = 'block';
+}
